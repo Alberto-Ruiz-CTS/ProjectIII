96cae9c72d54245e97a01a16ab284958bea98358
AlvaroCogn <Alvaro.Gimenez@Cognizant.com>
2024-07-03 13:14:00 +0000

    App runing again

--- model_testing_pytorch/app.py
+++ model_testing_pytorch/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import paths as p
 import functions_model_testing as f
+from model import FeaturesExtractorModel
 import os
 from rembg import remove
 from torchvision import models
@@ -29,12 +30,8 @@ def upload_and_display():
     st.image(image, width=400)  # Adjust width as needed
 
     #dict with models that are going to be tested and the image size they require as a tuple (model, size) (Different models may require different image sizes)
-    test_models = {
-      #"resnet50": (models.resnet50(pretrained=True), 224),
-      'efficientnet_b0': (models.efficientnet_b0(pretrained=True), 224),
-      #"vgg16": (models.vgg16(pretrained=True), 224),
-      #"googlenet": (models.googlenet(pretrained=True), 224)
-    }
+    test_models = ['vgg16','resnet50','efficient_net_b0']
+    metrics = ['cosine', 'euclidean', 'manhattan']
 
     # Read dictionary pkl file
     with open(p.dict_path + '/' + 'features_data.pkl', 'rb') as fp:
@@ -46,21 +43,19 @@ def upload_and_display():
       if input_path in feats[model_name]: 
         pass
       else:
-        model, size = test_models[model_name]
-        model = f.get_encoder(model_name)
-        input_feature = f.features_extraction(model, p.ext_path, size=size)
-        #feats[model_name][input_path] = input_feature[input_path]
+        featuresExtractorModel = FeaturesExtractorModel(model_name)
+        input_feature = f.features_extraction(featuresExtractorModel, p.ext_path)
         feats[model_name].update(input_feature)
 
-      for metrics in ['cosine', 'euclidean', 'manhattan']:
+      for metric in metrics:
         if inside_data == True:
-          sorted_similarities = f.similarity_extraction(input_path, feats[model_name], method=metrics)
+          sorted_similarities = f.similarity_extraction(input_path, feats[model_name], method=metric)
         else:
-          sorted_similarities = f.similarity_extraction(ext_img_path, feats[model_name], method=metrics)
-        sorted_sim_per_model[model_name + '_' + metrics] = sorted_similarities
-    
+          sorted_similarities = f.similarity_extraction(ext_img_path, feats[model_name], method=metric)
+        sorted_sim_per_model[model_name + '_' + metric] = sorted_similarities
+
     #Rank the best recommendations for each model
-    ranks = f.ranking_similarities(sorted_sim_per_model, top_n=10)
+    ranks = f.ranking_similarities(sorted_sim_per_model, metrics, test_models, top_n=10)
 
     #Create a rank fusion of all individuals rankings
     final_dict = f.reciprocal_rank_fusion(ranks)
