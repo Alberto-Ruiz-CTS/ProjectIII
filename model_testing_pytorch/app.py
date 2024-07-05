bc606c731de4f08cb44378911bac639e8f0a2edd
AlvaroCogn <Alvaro.Gimenez@Cognizant.com>
2024-07-05 08:23:05 +0000

    Create separate dicts for features with background and without and insert option in the app.

--- model_testing_pytorch/app.py
+++ model_testing_pytorch/app.py
@@ -8,8 +8,12 @@ from rembg import remove
 from torchvision import models
 import pickle
 
+st.set_page_config(layout="wide")
+
 def upload_and_display():
   size = 224
+  inside_data = False
+  sorted_sim_per_model = {}
   final_dict = {}
   recommended_images = []
   default_models = ['vgg16']
@@ -18,20 +22,17 @@ def upload_and_display():
   opt_metrics = ['cosine', 'euclidean', 'manhattan']
   opt_fusion = ['reciprocal', 'borda', 'relative score']
 
-  # Create the columns
-  col1, col2, col3 = st.columns(3)
+  dress_title, rec1_title, rec2_title, rec3_title = st.columns(4, vertical_alignment="top") 
+  dress, rec1, rec2, rec3 = st.columns(4, vertical_alignment="top") 
 
-  # Create the selectbox in the first column
-  with col1:
-    fusion_method = st.selectbox("Select an option:", opt_fusion, key="select_rank")
+  with st.sidebar:
+    fusion_method = st.selectbox("Select a fusion method:", opt_fusion, key="select_rank")
 
-  # Create the multiselect box in the second column
-  with col2:
-    metrics = st.multiselect("Select one or more options:", opt_metrics, default=default_metrics, key="mselect_metrics")
+    metrics = st.multiselect("Select one or more models:", opt_metrics, default=default_metrics, key="mselect_metrics")
 
-  # Create the multiselect box in the second column
-  with col3:
-    test_models = st.multiselect("Select one or more options:", opt_models, default=default_models, key="mselect_models")
+    test_models = st.multiselect("Select one or more models:", opt_models, default=default_models, key="mselect_models")
+
+    background = st.checkbox("Remove background")
 
 
   uploaded_image = st.file_uploader("Upload Image")
@@ -39,10 +40,19 @@ def upload_and_display():
     filename = uploaded_image.name
     image = Image.open(uploaded_image)
 
-    image = remove(image)
-    input_path = p.new_path + '/' + filename
-    ext_img_path = p.ext_path + '/' + filename
-    inside_data = False
+    #Remove background checbox functionality
+    if background:
+      image = remove(image)
+      input_path = p.new_path + '/' + filename
+      ext_path = p.ext_path + '_rb'
+      ext_img_path = ext_path + '/' + filename
+      pkl = 'features_data_rb.pkl'   
+    else:
+      input_path = p.data_path + '/' + filename
+      ext_path = p.ext_path
+      ext_img_path = p.ext_path + '/' + filename
+      pkl = 'features_data.pkl'
+      
     #Walk through the folder of the images and save it in the new folder without background
     for folder, subfolders, filenames in os.walk(p.new_path):
       for img in filenames:
@@ -50,20 +60,19 @@ def upload_and_display():
           image.save(ext_img_path, format='PNG')
         elif os.path.isfile(input_path):
           inside_data = True
-    st.image(image, width=200)  # Adjust width as needed
+    
+    image = image.resize((size, size))
+    dress_title.subheader("Your Dress")  # Add title or description 
+    dress.image(image, width=size)  # Adjust width as needed
 
     # Read dictionary pkl file
-    with open(p.dict_path + '/' + 'features_data.pkl', 'rb') as fp:
-        feats = pickle.load(fp)
+    with open(p.dict_path + '/' + pkl, 'rb') as fp:
+      feats = pickle.load(fp)
 
-    #Create a dict with the features for each model and metric combination
-    sorted_sim_per_model = {}
     for model_name in test_models:
-      if input_path in feats[model_name]: 
-        pass
-      else:
+      if input_path not in feats[model_name]: 
         featuresExtractorModel = FeaturesExtractorModel(model_name)
-        input_feature = f.features_extraction(featuresExtractorModel, p.ext_path)
+        input_feature = f.features_extraction(featuresExtractorModel, ext_path)
         feats[model_name].update(input_feature)
 
       for metric in metrics:
@@ -88,20 +97,22 @@ def upload_and_display():
       final_dict = f.relative_score_fusion(sorted_sim_per_model, metrics, test_models)
 
     for i, key in enumerate(final_dict.keys()):
-      if i < 3 and inside_data == False:
-        recommended_images.append(key)
-      elif inside_data == True and i != 0 and i < 4:
+      if i < 3 and inside_data == False or inside_data == True and i != 0 and i < 4:
         recommended_images.append(key)
 
-    col1, col2, col3 = st.columns(3)
     for i, recommended_image in enumerate(recommended_images):
-      col = [col1, col2, col3][i]
+      rec = [dress, rec1, rec2, rec3][i+1]
+      rec_title = [dress_title, rec1_title, rec2_title, rec3_title][i+1]
       try:
         # Attempt to open the recommended image (handle potential errors)
         rec_image = Image.open(recommended_image)
-        col.image(rec_image, width=200)
+        rec_image = rec_image.resize((size, size))
+        rec_title.subheader(f"Recommendation {i+1}")  # Add title or description 
+        rec.image(rec_image, width=size)  # Adjust width as needed
+
+        #col.image(rec_image, width=200)
       except FileNotFoundError:
-        col.write(f"Recommended Image {i+1} Not Found")
+        rec.write(f"Recommended Image {i+1} Not Found")
 
 st.title("Image Recommendation System")
 upload_and_display()
