20c8ea4796363dc013aef89fc43bea1451e5b70c
Alberto-Ruiz-Alejandro <alber.ruiz0@gmail.com>
2024-07-08 13:03:17 +0000

    Reestructuración del código

---
+++ src/config.py
@@ -0,0 +1,14 @@
+#Path to the input image
+input_path = "/teamspace/studios/this_studio/ProjectIII/data/fashion/dresses_no_bg/anarkali suit with lavender in color with intricate white patterns throughout the fabric.jpg"
+
+#Path to the dataset
+data_path = "/teamspace/studios/this_studio/ProjectIII/data/fashion/dresses"
+
+#Path to create new images without background
+new_path = '/teamspace/studios/this_studio/ProjectIII/data/fashion/dresses_no_bg'
+
+#Path to create a dictionary of the features of the dataset
+dict_path = '/teamspace/studios/this_studio/ProjectIII/data/fashion'
+
+#Path to create the image without background for a new images upload from the web
+ext_path = '/teamspace/studios/this_studio/ProjectIII/data/fashion/ext_im'
